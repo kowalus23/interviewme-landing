c74cf8f51ccac392add0b8c2993a9d94f2a23811
kowalus23 <kowalus23@gmail.com>
2019-08-18 11:31:24 +0200

    Updates

---
+++ precache-manifest.506e08131c63b264804e9dd41a37d31a.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "57f41ce471cd3c56a0addbd7d469e376",
+    "url": "/index.html"
+  },
+  {
+    "revision": "044aa4fcd79691db46ee",
+    "url": "/static/css/main.e1de234a.chunk.css"
+  },
+  {
+    "revision": "323337d96e3494a8d931",
+    "url": "/static/js/2.9f9aeec6.chunk.js"
+  },
+  {
+    "revision": "044aa4fcd79691db46ee",
+    "url": "/static/js/main.c45b58fd.chunk.js"
+  },
+  {
+    "revision": "5462dfdc97af0f463576",
+    "url": "/static/js/runtime~main.4304f76a.js"
+  },
+  {
+    "revision": "89cafa12ea2a4bbdae27d306bbf150d8",
+    "url": "/static/media/cake.89cafa12.svg"
+  },
+  {
+    "revision": "be2c178d5a325a090d3045ac16d20422",
+    "url": "/static/media/fishing-boat.be2c178d.svg"
+  },
+  {
+    "revision": "c071b35a374c8d807c8bb07a5fcd2847",
+    "url": "/static/media/forest.c071b35a.svg"
+  },
+  {
+    "revision": "2bacd99d9ae775158e9fdc9b85671dbb",
+    "url": "/static/media/game-controller.2bacd99d.svg"
+  },
+  {
+    "revision": "36acd45fd58ea882a98d1c231aa2c567",
+    "url": "/static/media/money-box.36acd45f.svg"
+  },
+  {
+    "revision": "a325dad9152629fb9be6e7c9598bf649",
+    "url": "/static/media/placeholder.a325dad9.png"
+  },
+  {
+    "revision": "19fb7eafdc41cb10f1b3bace7f44bac2",
+    "url": "/static/media/tent.19fb7eaf.svg"
+  }
+]);
